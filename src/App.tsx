57e5ae2c4ec2d005968d8cc41e10120bb035387d
SamExclamationPoint <126426968+SamExclamationPoint@users.noreply.github.com>
2026-08-04 12:09:39 -0600

    update import
    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 import "./index.css";
 import "./app.css";
 import UnlockScreen from "./components/UnlockScreen";
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import TabBar from "./TabBar";
 import HomeTab from "./HomeTab";
 import AITab from "./AITab";
